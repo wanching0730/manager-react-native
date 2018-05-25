6f4d8e27392eaa0d1ab6e2872bb00396b3e181cb
wanching_97 <wanching_97@hotmail.com>
2018-05-25 17:15:43 +0800

    setup firebase auth

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
 
 class App extends Component {
+
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyDD5ig-qSN7yXPJCnwrRe_AHkK5P_5JNkk",
+            authDomain: "manager-2989e.firebaseapp.com",
+            databaseURL: "https://manager-2989e.firebaseio.com",
+            projectId: "manager-2989e",
+            storageBucket: "",
+            messagingSenderId: "394955952877"
+          };
+
+          firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
